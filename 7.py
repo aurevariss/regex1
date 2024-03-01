437a13a7231852530d3609a454b208adcb7c583f
Aurevriss <goldyzlata2708@gmail.com>
2024-03-01 21:59:03 +0300

    fixed 7

--- 7.py
+++ 7.py
@@ -1,5 +1,11 @@
 import re
 
+def remover (lst_part, lst_all):
+    for j in lst_part:
+        if j in lst_all:
+            lst_all.remove(j)
+    return lst_all
+
 lpp = open("le ptit prince.txt", "r", encoding="utf-8")
 text_lpp = lpp.read()
 
@@ -8,13 +14,13 @@ list_of_common_words = ["Elles", "Voilà", "Aussi", "Quand", "Pourquoi", "Chapit
 
 all_capitals.extend(re.findall(r"[A-Z]\w{4,}\s?-?[A-Z]\w+|[A-Z]\w{4,}", text_lpp))
 all_capitals = list(dict.fromkeys(all_capitals))
+not_names = []
 
 for i in all_capitals:
     if re.search(r"\w+ez", i) or re.search(r"\w+ment", i) or re.search(r"\w+ons", i) or re.search(r"\w+iens", i):
-        all_capitals.remove(i)
+        not_names.append(i)
 
-for j in list_of_common_words:
-    if j in all_capitals:
-        all_capitals.remove(j)
+all_capitals = remover(not_names, all_capitals)
+all_capitals = remover(list_of_common_words, all_capitals)
 
 print(all_capitals)
