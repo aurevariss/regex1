daba6a595ab3db7a1985821fb85cf0a095a30d20
Aurevriss <goldyzlata2708@gmail.com>
2024-03-10 13:07:57 +0300

    8 added "un"

--- 8.py
+++ 8.py
@@ -6,7 +6,7 @@ text_lpp = lpp.read()
 masculins_back = []
 masculins = []
 masc_plus_adj = []
-masculins_back.extend(re.findall(r"\ble\s\w+|\bdu\s\w+|\bau\s\w+", text_lpp))
+masculins_back.extend(re.findall(r"\ble\s\w+|\bdu\s\w+|\bau\s\w+|\bun\s\w+", text_lpp))
 masc_plus_adj.extend(re.findall(r"petit\sw+|vieux\s\w+|grand\sw+|ancien\s\w+|nouveau\s\w+|nouvel\s\w+", text_lpp))
 masculins_back = list(dict.fromkeys(masculins_back))
 masc_plus_adj = list(dict.fromkeys(masc_plus_adj))
