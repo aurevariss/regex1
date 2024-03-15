216f132c8f803bb2ad0f6bae3f389fc521c84599
aurevariss <142241981+aurevariss@users.noreply.github.com>
2024-03-15 16:44:36 +0300

    pronoms added, less lists

--- 8.py
+++ 8.py
@@ -2,19 +2,20 @@ import re
 
 lpp = open("le ptit prince.txt", "r", encoding="utf-8")
 text_lpp = lpp.read()
+text_lpp = text_lpp.lower()
 
 masculins_back = []
 masculins = []
-masc_plus_adj = []
+masculins_pronoms_adj = []
 masculins_back.extend(re.findall(r"\ble\s\w+|\bdu\s\w+|\bau\s\w+|\bun\s\w+", text_lpp))
-masc_plus_adj.extend(re.findall(r"petit\sw+|vieux\s\w+|grand\sw+|ancien\s\w+|nouveau\s\w+|nouvel\s\w+", text_lpp))
+masculins_pronoms_adj.extend(re.findall(r"\b(?:m|t|s)on\s\w+|(?:n|v)otre\s\w+|leur\s\w+", text_lpp)).extend(re.findall(r"petit\sw+|vieux\s\w+|grand\sw+|ancien\s\w+|nouveau\s\w+|nouvel\s\w+", text_lpp))
 masculins_back = list(dict.fromkeys(masculins_back))
-masc_plus_adj = list(dict.fromkeys(masc_plus_adj))
+masc_pronoms_adj = list(dict.fromkeys(masc_plus_adj))
 
 for i in masculins_back:
     j = i[3:]
     masculins.append(j)
 
 masculins = list(dict.fromkeys(masculins))
-masculins.extend(masc_plus_adj)
+masculins.extend(masc_pronoms_adj)
 print(masculins)
